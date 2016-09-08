21d6ab13706939240d394ecd6b54a355c991a4d8
Chase3953 <chaseplayshd@gmail.com>
2016-09-07 19:41:22 -0500

    change password

---
+++ LooksGood/LooksGood/Account/ChangePassword.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessObjects;
+
+namespace LooksGood.Account
+{
+    public partial class ChangePassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["User"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        protected void btnChangePassword_Click(object sender, EventArgs e)
+        {
+            User user = (User)Session["User"];
+            if (txtOldPassword.Text == user.Password)
+            {
+                if (txtNewPassword.Text == txtConfirmPassword.Text)
+                {
+                    user.Password = txtNewPassword.Text;
+                    if (user.IsSavable() == true)
+                    {
+                        user.Save();
+                        Response.Redirect("Default.aspx");
+                    }
+                    else
+                    {
+                        //Show broken Rules
+                        foreach (BrokenRule br in user.BrokenRules.List)
+                        {
+                            AddCustomError(br.Rule);
+                        }
+                    }
+                }
+            }
+        }
+        private void AddCustomError(string message)
+        {
+            CustomValidator cv = new CustomValidator();
+            cv.ErrorMessage = message;
+            cv.Enabled = true;
+            cv.ValidationGroup = "vgChangePassword";
+            cv.IsValid = false;
+            Page.Validators.Add(cv);
+        }
+    }
+}
