7b5bf78ab08042e323cd8d2c1a460d78c0a2be98
chase3953 <chaseplayshd@gmail.com>
2016-09-09 19:16:26 -0500

    Got the account ddl working when logged in or logged out.

--- LooksGood/LooksGood/Site.Master.cs
+++ LooksGood/LooksGood/Site.Master.cs
@@ -10,8 +10,6 @@ using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 using BusinessObjects;
 
-//namespace LooksGood
-
     public partial class Site : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
@@ -19,52 +17,61 @@ using BusinessObjects;
             if (Session["User"] != null)
             {
                 Page.ClientScript.RegisterStartupScript(this.GetType(), "clientscript", "document.getElementById('ddlPreferences').style.visibility = 'visible';", true);
-
                 MasterPage masterpage = Page.Master;
                 // HyperLink hprCartItems = (HyperLink)masterpage.FindControl("hprCartItems");
                 //hprCartItems.Visible = true;
                 Page.ClientScript.RegisterStartupScript(this.GetType(), "clientscript", "document.getElementById('bsTop').style.visibility = 'visible';", true);
+                
 
                 User user = (User)Session["User"];
-                RemoveMenuItem("LOGIN");
-                ChangeMenuItem("WELCOME", String.Format("[Welcome {0}]", user.UserName));
+                Menu menu = (Menu)this.FindControl("Menu1");
+                //Button btn = (Button)masterpage.FindControl("btnUser");
+                //btn.Text = user.UserName;
+                
+
+                
+                //RemoveMenuItem("LOGIN");
+                //ChangeMenuItem("WELCOME", String.Format("[Welcome {0}]", user.UserName));
+
             }
             else
             {
                 Page.ClientScript.RegisterStartupScript(this.GetType(), "clientscript", "document.getElementById('ddlPreferences').style.visibility = 'hidden';", true);
-                RemoveMenuItem("LOGOUT");
-                RemoveMenuItem("WELCOME");
+                //RemoveMenuItem("LOGOUT");
+                //RemoveMenuItem("WELCOME");
 
                 MasterPage masterpage = Page.Master;
-               // HyperLink hprCartItems = (HyperLink)masterpage.FindControl("hprCartItems");
-               // hprCartItems.Visible = false;
+                // HyperLink hprCartItems = (HyperLink)masterpage.FindControl("hprCartItems");
+                // hprCartItems.Visible = false;
             }
         }
 
-        private void RemoveMenuItem(String text)
-        {
-            Menu mnu = (Menu)this.FindControl("Menu1");
-            MenuItem itemRemove = new MenuItem();
-            foreach (MenuItem menuItem in mnu.Items)
-            {
-                if (menuItem.Value == text)
-                {
-                    itemRemove = menuItem;
-                }
-            }
-            mnu.Items.Remove(itemRemove);
-        }
+        //private void RemoveMenuItem(String text)
+        //{
+        //    Menu mnu = (Menu)this.FindControl("Menu1");
+        //    MenuItem itemRemove = new MenuItem();
+        //    foreach (MenuItem menuItem in mnu.Items)
+        //    {
+        //        if (menuItem.Value == text)
+        //        {
+        //            itemRemove = menuItem;
+        //        }
+        //    }
+        //    mnu.Items.Remove(itemRemove);
+        //}
 
-        private void ChangeMenuItem(String value, String text)
-        {
-            Menu mnu = (Menu)this.FindControl("Menu1");
-            foreach (MenuItem menuItem in mnu.Items)
-            {
-                if (menuItem.Value == value)
-                {
-                    menuItem.Text = text;
-                }
-            }
-        }
+        //private void ChangeMenuItem(String value, String text)
+        //{
+        //    Menu mnu = (Menu)this.FindControl("Menu1");
+        //    foreach (MenuItem menuItem in mnu.Items)
+        //    {
+        //        if (menuItem.Value == value)
+        //        {
+        //            menuItem.Text = text;
+        //        }
+        //    }
+        //}
     }
+
+
 
