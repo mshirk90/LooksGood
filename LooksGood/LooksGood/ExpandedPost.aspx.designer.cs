193fb95495f0a5d4276b54bcd107428b9ed3a809
Chase3953 <chaseplayshd@gmail.com>
2016-09-15 20:30:33 -0500

    final changes.

--- LooksGood/LooksGood/ExpandedPost.aspx.designer.cs
+++ LooksGood/LooksGood/ExpandedPost.aspx.designer.cs
@@ -40,15 +40,6 @@ namespace LooksGood {
         protected global::System.Web.UI.WebControls.Image imgPost;
         
         /// <summary>
-        /// rptComments control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater rptComments;
-        
-        /// <summary>
         /// cmtComment control.
         /// </summary>
         /// <remarks>
@@ -65,5 +56,14 @@ namespace LooksGood {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnSubmit;
+        
+        /// <summary>
+        /// rptComments control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptComments;
     }
 }
