c9d5fb75f23e072d251410233ae666f754d0c487
Chase3953 <chaseplayshd@gmail.com>
2016-09-15 19:51:16 -0500

    commit

--- LooksGood/LooksGood/Account/Profile.aspx.designer.cs
+++ LooksGood/LooksGood/Account/Profile.aspx.designer.cs
@@ -11,5 +11,23 @@ namespace LooksGood.Account {
     
     
     public partial class Profile {
+        
+        /// <summary>
+        /// lblUserName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUserName;
+        
+        /// <summary>
+        /// imgProfile control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgProfile;
     }
 }
