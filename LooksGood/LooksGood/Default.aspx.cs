2fa891d2dcda323a0b65f34ff8f8d9243694b3fc
mshirk90 <mshirk90@gmail.com>
2016-10-20 19:27:29 -0500

    l

--- LooksGood/LooksGood/Default.aspx.cs
+++ LooksGood/LooksGood/Default.aspx.cs
@@ -5,21 +5,16 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using BusinessObjects;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace LooksGood
 {
     public partial class _Default : Page
     {
-
-       
-            
-
-
         protected void Page_Load(object sender, EventArgs e)
         {
             PostList postList = new PostList();
             postList = postList.GetMostRecent();
+            
             rptImages.DataSource = postList.List;
             rptImages.DataBind();
 
