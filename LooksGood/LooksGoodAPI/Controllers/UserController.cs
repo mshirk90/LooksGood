94a3018785f5eb915706aa3e81b3916dcc36d26f
Patrick Chase <Patrick Chase@MATT-PC>
2017-02-14 17:34:29 -0600

    changes

---
+++ LooksGood/LooksGoodAPI/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BusinessObjects;
+using DatabaseHelper;
+
+namespace LooksGoodAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        BusinessObjects.User user = new BusinessObjects.User
+        {            
+            //new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            //new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        //public IEnumerable<User> GetAllProducts()
+        //{
+        //    BusinessObjects.User user = new BusinessObjects.User();
+        //    return ;
+        //}
+
+        public IHttpActionResult GetUserById(Guid id)
+        {
+            User user = new BusinessObjects.User();
+            user = user.GetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
+
